7796a5921d1e3abff0d5f1b8e15fc60e8f9599ed
bm060658 <61071511+bm060658@users.noreply.github.com>
2020-04-26 12:50:12 +0700

    Add files via upload

--- compass.ino
+++ compass.ino
@@ -1,3 +1,6 @@
+#include <MMA8453_n0m1.h>
+
+
 #include <Wire.h>
 #include <MMA8453_n0m1.h>
  
@@ -6,7 +9,8 @@ MMA8453_n0m1 accel;
 void setup()
 {
 Serial.begin(9600);
-
+accel.setI2CAddr(0x1C); //change your device address if necessary, default is 0x1C
+accel.dataMode(true, 2); //enable highRes 10bit, 2g range [2g,4g,8g]
 Serial.println("XYZ Data Example");
 }
  
@@ -14,8 +18,6 @@ void loop()
 {
   
 accel.update();
-accel.setI2CAddr(0x1C); //change your device address if necessary, default is 0x1C
-accel.dataMode(true, 2); //enable highRes 10bit, 2g range [2g,4g,8g]
  
 Serial.print("x: ");
 Serial.print(accel.x());
